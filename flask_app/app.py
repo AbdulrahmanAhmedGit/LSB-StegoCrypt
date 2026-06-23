5ceb749b102aa60cf54476dac92c9c6b43ab530b
Abdulrahman Ahmed <ahmdhsnbdalrhmn357@gmail.com>
2026-06-23 09:15:27 +0300

    feat: configure app to use environment variables for port and network host

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
-
+PORT = os.getenv('PORT')
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host="0.0.0.0", port=PORT)
